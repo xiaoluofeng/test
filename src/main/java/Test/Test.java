e552ad8e5ea6dddd5366d7cc7f93a0b86ab7d1b0
xiaoluofeng <50603342+xiaoluofeng@users.noreply.github.com>
2019-05-14 16:03:07 +0800

    Update Test.java
    git 图形工具测试

--- src/main/java/Test/Test.java
+++ src/main/java/Test/Test.java
@@ -2,7 +2,7 @@ package Test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hhhh");
+		System.out.println("bbbb");
 		Utf();
 	}
 	public static void Utf(){
