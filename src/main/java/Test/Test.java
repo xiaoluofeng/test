145258f38030000e131ad3a3a978f21aaef3c900
xiaoluofeng <50603342+xiaoluofeng@users.noreply.github.com>
2019-05-14 15:42:24 +0800

    Update Test.java
    测试乱码

--- src/main/java/Test/Test.java
+++ src/main/java/Test/Test.java
@@ -2,7 +2,7 @@ package Test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("璁歌秴灏忔瓨姝�");
+		System.out.println("许超 小老弟");
 		Utf();
 	}
 	public static void Utf(){
