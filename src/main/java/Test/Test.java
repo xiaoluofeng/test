002ce0b2dd7b3734a875b5609e5d668f034006fd
Administrator <Administrator@luo>
2019-05-14 15:39:27 +0800

    测试乱码

--- src/main/java/Test/Test.java
+++ src/main/java/Test/Test.java
@@ -2,6 +2,10 @@ package Test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("许超小歘歘");
+		System.out.println("璁歌秴灏忔瓨姝�");
+		Utf();
+	}
+	public static void Utf(){
+		System.out.println("许超智障");
 	}
 }
