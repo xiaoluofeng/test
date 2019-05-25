4b575085fe4a26e040e79d362fb982b0619430d2
xiaoluofeng <luo2047929875@126.com>
2019-05-25 17:01:14 +0800

    Signed-off-by: xiaoluofeng <luo2047929875@126.com>

---
+++ src/main/java/Test/TestOne.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class Test {
+	public static void main(String[] args) {
+		Utf();
+		test();
+	}
+
+	public static void Utf() {
+		System.out.println("许超傻子");
+	}
+
+	public static void test() {
+		System.out.println("许超狗女人");
+	}
+}
